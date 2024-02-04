91b8566904e01db2a6c23cb113996e2489f810a4
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2024-02-04 16:22:04 -0600

    Create mutation of type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,15 @@
 import React from "react";
+import { Disposable, UseMutationConfig } from "react-relay";
+import { MutationParameters } from "relay-runtime";
 
 export type ValueOf<T> = T[keyof T];
+
+/**
+ * @description Create a type from a Relay mutation
+ * @example type UserContextTypes = {
+ *   createUser: MutationTypeOf<createUserMutation>
+ * }
+ */
+export type MutationTypeOf<T extends MutationParameters> = (
+  config: UseMutationConfig<T>
+) => Disposable;
